9f3e0bfb13bbe7c52ece026f2f5603d8d92a4942
florstefani0204 <florstefani0204@gmail.com>
2025-03-20 20:11:35 -0300

    adicição da finção de soma

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
+void somatoria (int x, int y){
+    int i; 
+    long int soma=0;
+    for (i=x; i<=y; i++){
+        soma = soma +1;
+    }
+    printf("\nA somatoria é %ld\n", soma);
+}
+
 int main (void){
+    int x, y;
     printf("\n\n :::::::::::::::::: INICIO DO PROGRAMA ::::::::::::::::\n\n");
+    printf("\nDigite x");
+    scanf("%d",&x);
+    printf("\nDigite y");
+    scanf("%d",&y);
+
+    soma(x, y);
+    
     return 0;
 }
